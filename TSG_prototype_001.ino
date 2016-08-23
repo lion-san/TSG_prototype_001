db0a057f9c3aa86b433e91e59aa1a1415048a93d
yokoi.shinya <shinya.yokoi@gmail.com>
2016-08-24 08:45:44 +0900

    calman create

--- TSG_prototype_001.ino
+++ TSG_prototype_001.ino
@@ -83,7 +83,7 @@ void loop(void) {                               //LCD描画
   printGyro();  // Print "G: gx, gy, gz"　　　シリアルモニタ表示用フォーマット
   printAccel(); // Print "A: ax, ay, az"
   printMag();   // Print "M: mx, my, mz"
-  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+  printAttitude (imu.gx, imu.gy, imu.gz, imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
   Serial.println();
 
 }
@@ -174,7 +174,7 @@ void printMag()
 }
 //---------------------------------------------------------
 
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+void printAttitude(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
 {
 
   //重力加速度から求めた角度ををカルマンフィルタの初期値とする
@@ -212,6 +212,41 @@ void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
 
   hedVal = (heading);
 
+}
+/**
+ * カルマンフィルタの計算式
+ */
+float Q_angle  =  0.001;
+float Q_gyro   =  0.003;
+float R_angle  =  0.03; 
+float x_angle;
+float x_bias = 0;
+float P_00 = 0, P_01 = 0, P_10 = 0, P_11 = 0;
+float  y, S;
+float K_0, K_1;
+float dt=0.005;
+float kal_deg;
 
+float kalmanCalculate(float newAngle, float newRate){
+                
+        x_angle += dt * (newRate - x_bias);
+       
+        P_00 +=  dt * (dt * P_11 - P_01 - P_10 + Q_angle);
+        P_01 -=  dt * P_11;
+        P_10 -=  dt * P_11;
+        P_11 +=  Q_gyro * dt;
 
+        y = newAngle - x_angle;
+        S = P_00 + R_angle;
+        K_0 = P_00 / S;
+        K_1 = P_10 / S;
+
+        x_angle +=  K_0 * y;
+        x_bias  +=  K_1 * y;
+        P_00 -= K_0 * P_00;
+        P_01 -= K_0 * P_01;
+        P_10 -= K_1 * P_00;
+        P_11 -= K_1 * P_01;
+
+    return x_angle;
 }
