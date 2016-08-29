ff8b74822d1daa87f6dee2f9313715e481542456
yokoi.shinya <shinya.yokoi@gmail.com>
2016-08-29 14:08:35 +0900

    Add SDCARD TEST

--- TSG_prototype_001.ino
+++ TSG_prototype_001.ino
@@ -16,12 +16,34 @@
 //
 //　　　　
 //----------------------------------------------------------//
+/*
+  SD card datalogger
 
+ This example shows how to log data from three analog sensors
+ to an SD card using the SD library.
+
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+
+ created  24 Nov 2010
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
 
 
 #include <SPI.h>                                //SPIライブラリ
 #include <Wire.h>                               //I2Cライブラリ
 #include <SparkFunLSM9DS1.h>                  //LSM9DS1ライブラリ：https://github.com/sparkfun/LSM9DS1_Breakout
+
+#include <SD.h>
 
 
 
@@ -31,6 +53,10 @@ LSM9DS1 imu;
 
 int SAMPLETIME = 10;
 
+//MicroSD 
+//const int chipSelect = 4;//Arduino UNO
+const int chipSelect = 10;//Arduino Micro
+
 #define LSM9DS1_M  0x1E // SPIアドレス設定 0x1C if SDO_M is LOW
 #define LSM9DS1_AG  0x6B // SPIアドレス設定 if SDO_AG is LOW
 
@@ -39,6 +65,7 @@ int SAMPLETIME = 10;
 #define PRINT_SPEED 250 // 250 ms between prints
 #define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
 //-------------------------------------------------------------------------
+/*
 float s = 0;                                    //表示用データレジスタ
 float gxVal = 0;                                //ジャイロｘ軸用データーレジスタ
 float gyVal = 0;                                //ジャイロｙ軸用データーレジスタ
@@ -50,21 +77,39 @@ float mxVal = 0;                                //Mag x 用データーレジス
 float myVal = 0;                                //Mag ｙ 用データーレジスタ
 float mzVal = 0;                                //Mag x 用データーレジスタ
 float hedVal = 0;                               //Hedding 用データーレジスタ
-
+*/
 //------------------------------------------------------------------------
 
 //----------------------------------------------------------------------
 void setup(void) {
 
+  // Open serial communications and wait for port to open:
+  //Serial.begin(9600);
+  /*while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }*/
+
 
   Serial.begin(115200);                                 //シリアルモニタ通信速度設定
 
+
+  //SD Card Initialize ====================================
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
+  //=======================================================
+
+  //LSM9DS1 Initialize=====================================
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
-
-
-
 
   if (!imu.begin())                                     //センサ接続エラー時の表示
 
@@ -78,7 +123,7 @@ void setup(void) {
     while (1)
       ;
   }
-
+  //=======================================================
 }
 
 //-----------------------------------------------------------------
@@ -90,8 +135,42 @@ void loop(void) {                               //LCD描画
   printGyro();  // Print "G: gx, gy, gz"　　　シリアルモニタ表示用フォーマット
   printAccel(); // Print "A: ax, ay, az"
   printMag();   // Print "M: mx, my, mz"
-  printAttitude (imu.calcGyro(imu.gx), imu.calcGyro(imu.gy), imu.calcGyro(imu.gz), imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+  String record = 
+    printAttitude (imu.calcGyro(imu.gx), imu.calcGyro(imu.gy), imu.calcGyro(imu.gz), imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
   Serial.println();
+
+
+  //Write MicroSD =================================
+    // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  /*for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }*/
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(record);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(record);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+
+  //======================================================
+
 
 }
 
@@ -200,8 +279,10 @@ void printMag()
  //相補フィルタの保持値
  float prev_pitch = 0.0;
  float prev_roll = 0.0;
-void printAttitude(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
+String printAttitude(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
 {
+
+  String output = "";
 
   //重力加速度から求めた角度ををカルマンフィルタの初期値とする
   float roll = atan2(ay, az);
@@ -255,11 +336,15 @@ void printAttitude(float gx, float gy, float gz, float ax, float ay, float az, f
   prev_pitch = complementFilter( prev_pitch, gyro_x, pitch );
   prev_roll = complementFilter( prev_roll, gyro_y, roll );
 
+  output = "hoge";
+
   Serial.print("Pitch, Roll: ");
   Serial.print(prev_pitch, 2);
   Serial.print(", ");
   Serial.print(prev_roll, 2);
   Serial.println("");
+
+  return output;
 }
 
 
@@ -277,7 +362,7 @@ void printAttitude(float gx, float gy, float gz, float ax, float ay, float az, f
 /**
  * カルマンフィルタの計算式
  */
-float Q_angle  =  0.001;
+/*float Q_angle  =  0.001;
 float Q_gyro   =  0.003;
 float R_angle  =  0.03; 
 float x_angle;
@@ -310,4 +395,4 @@ float kalmanCalculate(float newAngle, float newRate){
         P_11 -= K_1 * P_01;
 
     return x_angle;
-}
+}*/
